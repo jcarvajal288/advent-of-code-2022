51f04ddfd170c584485d237e3ccf36eb1498cc19
Max Carvajal <jcarvajal@crimson.ua.edu>
2022-12-03 14:33:29 -0600

    Day 3 part 2 complete

--- src/advent_of_code_2022/day3.clj
+++ src/advent_of_code_2022/day3.clj
@@ -2,8 +2,11 @@
   (:require [advent-of-code-2022.util :refer :all]
     [taoensso.timbre :as log]))
 
+(defn in-all-vectors? [ch vectors]
+  (every? #(some #{ch} %) vectors))
+
 (defn find-common-element [vectors]
-  (some #(some #{%} (last vectors)) (first vectors)))
+  (some #(when (in-all-vectors? % (rest vectors)) %) (first vectors)))
 
 (defn get-priority [ch]
   (let [ch-dec (int ch)]
@@ -15,6 +18,22 @@
   (let [contents (vec (get-resource-file-by-line file))]
     (->> contents
          (map #(split-at (/ (count %) 2) %))
+         (map find-common-element)
+         (map get-priority)
+         (reduce +))))
+
+(defn- group-by-threes-reducer [items state]
+  (if (<= (count items) 3)
+      (conj state items)
+      (group-by-threes-reducer (drop 3 items) (conj state (vec (take 3 items))))))
+
+(defn group-by-threes [items]
+  (group-by-threes-reducer items []))
+
+(defn sum-priorities-of-group-badges [file]
+  (let [contents (vec (get-resource-file-by-line file))]
+    (->> contents
+         (group-by-threes)
          (map find-common-element)
          (map get-priority)
          (reduce +))))
--- test/advent_of_code_2022/day3_test.clj
+++ test/advent_of_code_2022/day3_test.clj
@@ -11,9 +11,26 @@
   (testing "day3-full-test"
     (is (= (sum-priorities "day3-full-data.txt") 7763))))
 
+(deftest day3-sample-test-part2
+  (testing "day3-sample-test-part2"
+    (is (= (sum-priorities-of-group-badges "day3-example-data.txt") 70))))
+
+(deftest day3-full-test-part2
+  (testing "day3-full-test-part2"
+    (is (= (sum-priorities-of-group-badges "day3-full-data.txt") 2569))))
+
+
 (deftest find-common-element-test
   (testing "find-common-element"
     (is (= (find-common-element [[\a \b \c] [\d \c \e]]) \c))))
+
+(deftest find-common-element-test-many
+  (testing "find-common-element-many"
+    (is (= (find-common-element [[\a \b \c] [\d \c \e] [ \f \g \c]]) \c))))
+
+(deftest group-by-threes-test
+  (testing "group-by-threes"
+    (is (= (group-by-threes [1 2 3 4 5 6 7 8 9]) [[1 2 3] [4 5 6] [7 8 9]]))))
 
 (deftest get-priority-test
   (testing "get-priority-test"
