4238106e3a4c1cbf33ee15402a453db47e37f3b1
Max Carvajal <jcarvajal@crimson.ua.edu>
2023-01-28 13:58:14 -0600

    WIP: code cleanup; before stack overflow in dir-size

--- src/advent_of_code_2022/day7.clj
+++ src/advent_of_code_2022/day7.clj
@@ -36,16 +36,15 @@
 (defn- cdt-reducer [tree current-dir commands]
   (let [cmd (first commands)
         tokens (if (= cmd nil) nil (str/split cmd #" "))]
-    (log/info "-----")
-    (log/info tree)
-    (log/info cmd)
-    (log/info (str "current-dir: " current-dir))
     (cond
       (empty? commands) tree
       (str/starts-with? cmd "$ cd") (cd tree current-dir tokens commands)
       (= (first tokens) "dir") (dir tree tokens current-dir commands)
       (is-number? (first tokens)) (add-file tree current-dir tokens commands)
       :else (cdt-reducer tree current-dir (rest commands)))))
+
+(defn construct-dir-tree [commands]
+  (cdt-reducer {"/" (->Directory "/" nil [])} "/" commands))
 
 (defn- cd [tree current-dir tokens commands]
   (let [dir-name (last tokens)
@@ -58,7 +57,6 @@
   (let [current-node (get tree current-dir)
         new-dir-name (last tokens)
         new-dir (->Directory new-dir-name current-dir [])]
-    (log/info (str "current-node: " (.name current-node)))
     (cdt-reducer
       (if (contains? tree  new-dir-name)
         tree
@@ -75,18 +73,12 @@
         file-name (last tokens)
         new-file (->File file-name (:parent current-node) file-size)
         tree-with-new-file (add-file-to-tree tree new-file)]
-    (log/info tree)
-    (log/info tree-with-new-file)
-    ;(log/info current-node)
     (cdt-reducer
       (assoc tree-with-new-file current-dir (->Directory (.name current-node)
                                            (.parent current-node)
                                            (conj (.children current-node) new-file)))
       current-dir
       (rest commands))))
-
-(defn construct-dir-tree [commands]
-  (cdt-reducer {"/" (->Directory "/" nil [])} "/" commands))
 
 (defn dir-size-reducer [total-size node-name dir-tree]
   (let [current-node (get-node dir-tree node-name)]
@@ -100,12 +92,12 @@
 
 (defn total-dir-size [size-limit commands]
   (let [dir-tree (construct-dir-tree commands)]
-  (->> dir-tree
-       (vals)
-       (filter #(instance? Directory %))
-       (map #(.name %))
-       (map #(dir-size % dir-tree))
-       (filter #(<= % size-limit))
-       (reduce +)
-       )))
+    (->> dir-tree
+         (vals)
+         (filter #(instance? Directory %))
+         (map #(.name %))
+         (map #(dir-size % dir-tree))
+         (filter #(<= % size-limit))
+         (reduce +)
+         )))
 
