90469e02458332d09c4f6003ac23688b063b7a2f
Max Carvajal <jcarvajal@crimson.ua.edu>
2023-03-19 15:09:32 -0500

    WIP: Day 13 part 2

--- src/advent_of_code_2022/day13.clj
+++ src/advent_of_code_2022/day13.clj
@@ -4,7 +4,13 @@
             ))
 
 (defn compare-integers [pair]
+  (format "  Comparing %d vs %d" (first pair) (last pair))
   (<= (first pair) (last pair)))
+
+(defn unbox [value]
+  (if (integer? value)
+    value
+    (unbox (first value))))
 
 (defn compare-lists [lists]
   (let [left (first lists)
@@ -12,18 +18,27 @@
         list-left (if (integer? left) [left] left)
         list-right (if (integer? right) [right] right)
         zipped (zip list-left list-right)
-        first-unequal-pair (first (filter #(not (= (first %) (last %))) zipped))]
+        first-unequal-pair (first (filter #(not (= (unbox (first %)) (unbox (last %)))) zipped))]
+    (log/info (format "  Comparing %s vs %s" list-left list-right))
+    (log/info (str "first unequal pair: " first-unequal-pair))
     (cond
-      (and (= (first list-left) list-right) (> (count list-left) (count list-right))) false
-      (and (= [(first list-left)] (first list-right)) (> (count list-left) (count list-right))) false
+      ;(and (= (first list-left) list-right) (> (count list-left) (count list-right))) false
+      ;(and (= [(first list-left)] (first list-right)) (> (count list-left) 1) (= (count list-right) 1)) false
+      ;(and (= [(first list-left)] (first list-right)) (> (count list-left) (count list-right))) false
       (empty? first-unequal-pair) (<= (count list-left) (count list-right))
       (every? integer? first-unequal-pair) (compare-integers first-unequal-pair)
       :else (recur first-unequal-pair))))
 
 (defn packet-in-right-order? [packet-strings]
   (let [packet1 (load-string (first packet-strings))
-        packet2 (load-string (last packet-strings))]
-    (compare-lists [packet1 packet2])))
+        packet2 (load-string (last packet-strings))
+        result (compare-lists [packet1 packet2])]
+    (log/info (str "    Result: " result))
+    result))
+
+(defn compare-packets [packet1 packet2]
+  (log/info (format "Comparing packets %s vs %s" packet1 packet2))
+  (packet-in-right-order? [packet1 packet2]))
 
 (defn get-packet-pairs [filename]
   (->> (get-resource-file-by-line filename)
@@ -38,3 +53,15 @@
        (map first)
        (map inc)
        (reduce +)))
+
+(defn sort-packets [filename]
+  (->> (get-resource-file-by-line filename)
+       (filter #(not (empty? %)))
+       (concat ["[[2]]" "[[6]]"])
+       (sort compare-packets)))
+
+(defn product-of-indices [filename]
+  (let [sorted-packets (sort-packets filename)
+        two-index (inc (.indexOf sorted-packets "[[2]]"))
+        six-index (inc (.indexOf sorted-packets "[[6]]"))]
+    (* two-index six-index)))
