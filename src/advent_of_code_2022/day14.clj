7b9daeac6bd593d4761bfdef4d050cd243775ed5
Max Carvajal <maxc@maxc-a01.vmware.com>
2023-03-22 17:00:31 -0500

    Day 14 Part 1 complete

--- src/advent_of_code_2022/day14.clj
+++ src/advent_of_code_2022/day14.clj
@@ -6,10 +6,15 @@
 (defn initialize-2d-vector [width height value]
   (vec (repeat height (vec (repeat width value)))))
 
-(defn set-2d-coord [grid x y value]
-  (let [old-row (nth grid y)
+(defn set-2d-coord [grid coord value]
+  (let [x (first coord)
+        y (last coord)
+        old-row (nth grid y)
         new-row (assoc old-row x value)]
     (assoc grid y new-row)))
+
+(defn get-2d-coord [grid coord]
+  (nth (nth grid (last coord)) (first coord)))
 
 (defn parse-rock-paths [filename]
   (->> (get-resource-file-by-line filename)
@@ -33,11 +38,19 @@
 (defn get-all-points-between [p1 p2]
   (if (= (first p1) (first p2))
     (if (<= (last p1) (last p2))
-      (rest (map (fn [y] [(first p1) y]) (range (last p1) (inc (last p2)))))
-      (map (fn [y] [(first p1) y]) (range (last p1) (dec (last p2)) -1)))
+      (rest (map
+              (fn [y] [(first p1) y])
+              (range (last p1) (inc (last p2)))))
+      (map
+        (fn [y] [(first p1) y])
+        (range (last p1) (dec (last p2)) -1)))
     (if (<= (first p1) (first p2))
-      (rest (map (fn [x] [x (last p1)]) (range (first p1) (inc (first p2)))))
-      (map (fn [x] [x (last p1)]) (range (first p1) (dec (first p2)) -1)))))
+      (rest (map
+              (fn [x] [x (last p1)])
+              (range (first p1) (inc (first p2)))))
+      (map
+        (fn [x] [x (last p1)])
+        (range (first p1) (dec (first p2)) -1)))))
 
 (defn expand-path [path]
   (reduce (fn [expanded-path coord]
@@ -48,9 +61,9 @@
 (defn apply-rock-path [rock-map rock-path]
   (let [expanded-path (expand-path rock-path)]
     (reduce (fn [rock-map coord]
-              (set-2d-coord rock-map (first coord) (last coord) \#))
-              rock-map
-              expanded-path)))
+              (set-2d-coord rock-map coord \#))
+            rock-map
+            expanded-path)))
 
 (defn apply-rock-paths [rock-paths rock-map]
   (reduce (fn [rock-map rock-path]
@@ -64,3 +77,26 @@
         height (find-dimension rock-paths :height)]
     (->> (initialize-2d-vector (inc width) (inc height) \.)
          (apply-rock-paths rock-paths))))
+
+(defn drop-sand [rock-map sand-coord]
+  (let [x (first sand-coord)
+        y (last sand-coord)
+        down-left [(dec x) (inc y)]
+        down [x (inc y)]
+        down-right [(inc x) (inc y)]
+        is-air? #(= % \.)]
+    (cond
+      (>= (inc y) (count rock-map)) :void-reached
+      (is-air? (get-2d-coord rock-map down)) (drop-sand rock-map down)
+      (is-air? (get-2d-coord rock-map down-left)) (drop-sand rock-map down-left)
+      (is-air? (get-2d-coord rock-map down-right)) (drop-sand rock-map down-right)
+      :else (set-2d-coord rock-map sand-coord \o))))
+
+(defn run-simulation [sand-outlet filename]
+  (let [rock-map (build-map filename)]
+    (reduce (fn [rock-map num-sand]
+              (if (= rock-map :void-reached)
+                (reduced (dec num-sand))
+                (drop-sand rock-map sand-outlet)))
+            rock-map
+            (range))))
--- test/advent_of_code_2022/day14_test.clj
+++ test/advent_of_code_2022/day14_test.clj
@@ -1,3 +1,11 @@
 (ns advent-of-code-2022.day14-test
   (:require [clojure.test :refer :all]
             [advent-of-code-2022.day14 :refer :all]))
+
+(deftest day14-example-test
+  (testing "day14-example-test"
+    (is (= (run-simulation [500 0] "day14-example-data.txt") 24))))
+
+(deftest day14-full-test
+  (testing "day14-full-test"
+    (is (= (run-simulation [500 0] "day14-full-data.txt") 1406))))
